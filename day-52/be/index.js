d57fd74da247b7921ec7135bc511f38603fe7b45
tugsoo19 <tugswork@gmail.com>
2023-01-30 22:38:46 +0800

    day-52 update section updated

--- day-52/be/index.js
+++ day-52/be/index.js
@@ -1,108 +1,144 @@
-const express = require('express')
-const cors = require('cors')
-const fs = require('fs')
-const { log } = require('console')
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const { log } = require("console");
 
-const app = express()
-const PORT = 8080
+const app = express();
+const PORT = 8080;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/users', (request, response) => {
-
-    fs.readFile('./data/users.json', 'utf-8', (readError, readData) => {
-        if (readError) {
-            response.json({
-                status: 'file reader error',
-                data: []
-            })
-        }
-
-        const objectData = JSON.parse(readData)
-
-        response.json({
-            status: 'success',
-            data: objectData
-        })
-    })
-
-})
-
-app.post("/users", (request, response) => {
-    const body = request.body
-    console.log(body);
-
-    const newUser = {
-        id: Date.now().toString(),
-        username: body.username,
-        age: body.age
+app.get("/users", (request, response) => {
+  fs.readFile("./data/users.json", "utf-8", (readError, readData) => {
+    if (readError) {
+      response.json({
+        status: "file reader error",
+        data: [],
+      });
     }
 
-    fs.readFile('./data/users.json', 'utf-8', (readError, readData) => {
-        if (readError) {
-            response.json({
-                status: 'file does not exist',
-                data: []
-            })
+    const objectData = JSON.parse(readData);
+
+    response.json({
+      status: "success",
+      data: objectData,
+    });
+  });
+});
+
+app.post("/users", (request, response) => {
+  const body = request.body;
+  console.log(body);
+
+  const newUser = {
+    id: Date.now().toString(),
+    username: body.username,
+    age: body.age,
+  };
+
+  fs.readFile("./data/users.json", "utf-8", (readError, readData) => {
+    if (readError) {
+      response.json({
+        status: "file does not exist",
+        data: [],
+      });
+    }
+    // console.log(readData);
+    // console.log(typeof readData);
+
+    const dataObject = JSON.parse(readData);
+    // console.log(typeof dataObject);
+    console.log(dataObject);
+    console.log("======");
+    dataObject.push(newUser);
+    console.log(dataObject);
+
+    fs.writeFile(
+      "./data/users.json",
+      JSON.stringify(dataObject),
+      (writeError) => {
+        if (writeError) {
+          response.json({
+            status: "Error during file write",
+            data: [],
+          });
         }
-        // console.log(readData);
-        // console.log(typeof readData);
-
-
-        const dataObject = JSON.parse(readData)
-        // console.log(typeof dataObject);
-        console.log(dataObject);
-        console.log('======');
-        dataObject.push(newUser)
-        console.log(dataObject);
-
-        fs.writeFile('./data/users.json', JSON.stringify(dataObject), (writeError) => {
-            if (writeError) {
-                response.json({
-                    status: 'Error during file write',
-                    data: []
-                })
-            }
-            response.json({
-                status: "success",
-                data: dataObject,
-            })
-        })
-    })
-})
+        response.json({
+          status: "success",
+          data: dataObject,
+        });
+      }
+    );
+  });
+});
 
 app.delete("/users", (request, response) => {
-    const body = request.body
-    fs.readFile('./data/users.json', 'utf-8', (readError, readData) => {
-        if (readError) {
-            response.json({
-                status: 'file reader error',
-                data: []
-            })
+  const body = request.body;
+  fs.readFile("./data/users.json", "utf-8", (readError, readData) => {
+    if (readError) {
+      response.json({
+        status: "file reader error",
+        data: [],
+      });
+    }
+    const readObject = JSON.parse(readData);
+
+    const filteredObjects = readObject.filter((o) => o.id !== body.userId);
+
+    fs.writeFile(
+      "./data/users.json",
+      JSON.stringify(filteredObjects),
+      (writeError) => {
+        if (writeError) {
+          response.json({
+            status: "write file error",
+            data: [],
+          });
         }
-        const readObject = JSON.parse(readData)
+        response.json({
+          status: "success",
+          data: filteredObjects,
+        });
+      }
+    );
+  });
+});
 
-        const filteredObjects = readObject.filter(o => o.id !== body.userId)
-
-        fs.writeFile('./data/users.json', JSON.stringify(filteredObjects), (writeError) => {
-            if (writeError) {
-                response.json({
-                    status: 'write file error',
-                    data: []
-                })
-            }
-            response.json({
-                status: 'success',
-                data: filteredObjects
-            })
-        })
-
-    })
-
-})
-
+app.put("/users", (request, response) => {
+  const body = request.body;
+  fs.readFile("./data/users.json", "utf-8", (readError, readData) => {
+    const savedData = JSON.parse(readData);
+    if (readError) {
+      response.json({
+        status: "read file error",
+      });
+    }
+    const updatedData = savedData.map((d) => {
+      if (d.id === body.id) {
+        (d.username = body.username), (d.age = body.age);
+      }
+      return d;
+    });
+    fs.writeFile(
+      "./data/users.json",
+      JSON.stringify(updatedData),
+      (writeError) => {
+        if (writeError) {
+          response.json({
+            status: "Error during file write",
+            data: [],
+          });
+        }
+        response.json({
+          status: "success",
+          data: updatedData,
+        });
+      }
+    );
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- day-52/fe/src/App.js
+++ day-52/fe/src/App.js
@@ -1,80 +1,103 @@
-
-import './App.css';
-import { useEffect, useState } from 'react'
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  const URL = 'http://localhost:8080/users';
+  const URL = "http://localhost:8080/users";
 
-  const [users, setUsers] = useState([])
-  const [openForm, setOpenForm] = useState(false)
-  const [currentUser, setCurrentUser] = useState({})
+  const [users, setUsers] = useState([]);
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [currentUser, setCurrentUser] = useState({
+    id: "",
+    username: "",
+    age: "",
+    test: "",
+  });
 
   async function fetchAllData() {
     // fetch a data from localhost:8080/users
-    const FETCHED_DATA = await fetch(URL) // response
-    const FETCHED_JSON = await FETCHED_DATA.json()  // {status: 'success', data:[{id:...}]}
-    setUsers(FETCHED_JSON.data)
-
+    const FETCHED_DATA = await fetch(URL); // response
+    const FETCHED_JSON = await FETCHED_DATA.json(); // {status: 'success', data:[{id:...}]}
+    setUsers(FETCHED_JSON.data);
   }
 
   useEffect(() => {
-    fetchAllData()
-  }, [])
+    fetchAllData();
+  }, []);
 
   async function handleSubmit(e) {
     e.preventDefault();
-    const postData = {
-      username: e.target.username.value,
-      age: e.target.age.value
+    if (isUpdate) {
+      update();
+      console.log(isUpdate);
+    } else {
+      const postData = {
+        username: e.target.username.value,
+        age: e.target.age.value,
+      };
+
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+      };
+
+      const FETCHED_DATA = await fetch(URL, options);
+      const FETCHED_JSON = await FETCHED_DATA.json();
+      console.log(FETCHED_JSON);
+      setUsers(FETCHED_JSON.data);
     }
-
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(postData)
-    }
-
-    const FETCHED_DATA = await fetch(URL, options)
-    const FETCHED_JSON = await FETCHED_DATA.json()
-    console.log(FETCHED_JSON);
-    setUsers(FETCHED_JSON.data)
-
   }
 
   async function handleDelete(userId) {
     const options = {
-      method: 'DELETE',
+      method: "DELETE",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        userId: userId
-      })
-    }
-    const FETCHED_DATA = await fetch(URL, options)
-    const FETCHED_JSON = await FETCHED_DATA.json()
-    setUsers(FETCHED_JSON.data)
+        userId: userId,
+      }),
+    };
+    const FETCHED_DATA = await fetch(URL, options);
+    const FETCHED_JSON = await FETCHED_DATA.json();
+    setUsers(FETCHED_JSON.data);
+  }
+  async function update() {
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(currentUser),
+    };
+    const FETCHED_DATA = await fetch(URL, options);
+    const FETCHED_JSON = await FETCHED_DATA.json();
+    setUsers(FETCHED_JSON.data);
+  }
+
+  function handleNameChange(e) {
+    setCurrentUser({
+      ...currentUser,
+      username: e.target.value,
+    });
+    console.log(setCurrentUser);
+  }
+
+  function handleAgeChange(e) {
+    setCurrentUser({
+      ...currentUser,
+      age: e.target.value,
+    });
   }
 
   async function handleEdit(userId) {
-    // const options = {
-    //   method: 'EDIT',
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify({
-    //     userId: userId
-    //   })
-    // }
-    // const FETCHED_DATA = await fetch(URL, options)
-    // const FETCHED_JSON = await FETCHED_DATA.json()
-    // setUsers(FETCHED_JSON.data)
-    setCurrentUser(setUsers)
-    setOpenForm(true)
+    setIsUpdate(true);
+    const current = users.filter((user) => user.id === userId);
+    setCurrentUser(current);
+    console.log(setCurrentUser);
   }
-
 
   return (
     <div className="App">
@@ -83,30 +106,37 @@ function App() {
       <form onSubmit={handleSubmit}>
         <label>
           User Name:
-          <input name="username" />
+          <input
+            name="username"
+            value={currentUser.username}
+            onChange={handleNameChange}
+          />
         </label>
         <br />
         <label>
           Age:
-          <input name="age" />
+          <input
+            name="age"
+            value={currentUser.age}
+            onChange={handleAgeChange}
+          />
         </label>
-        {openForm ? setUsers = { setUsers } : <div></div>}
         <br />
-        <button>Submit</button>
+        <button>{isUpdate ? "Update" : "Submit"}</button>
       </form>
       <h3>Users List</h3>
-      {
-        users && users.map(user => {
+      {users &&
+        users.map((user) => {
           return (
             <div>
-              <p>{user.username} : {user.age}  {" "}
-                <button onClick={() => handleEdit(user.id)}>Edit</button>
-                <button onClick={() => handleDelete(user.id)}>Delete</button></p>
-
+              <p>
+                {user.username} : {user.age}{" "}
+                <button onClick={() => handleEdit(user.id)}>Edit</button>{" "}
+                <button onClick={() => handleDelete(user.id)}>Delete</button>
+              </p>
             </div>
-          )
-        })
-      }
+          );
+        })}
     </div>
   );
 }
