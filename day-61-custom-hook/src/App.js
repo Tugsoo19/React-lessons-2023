53ba5b59b35f02565e446297f17b00d3bb1e0d5d
Tugsoo19 <tugswork@gmail.com>
2023-02-13 12:32:45 +0800

    custom hook

--- day-61-custom-hook/src/App.js
+++ day-61-custom-hook/src/App.js
@@ -1,5 +1,9 @@
 
 import './App.css';
+import Body from './components/Body';
+import ChangeTheme from './components/ChangeTheme';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 import TimersDashboard from "./components/TimersDashboard";
 import { TimerContextProvider } from './context/TimerContext';
@@ -7,10 +11,14 @@ import { TimerContextProvider } from './context/TimerContext';
 export default function App() {
   return (
     <div>
-      <h1>Timer App</h1>
-      <TimerContextProvider>
+      <h1>Day-61 Custom Hook</h1>
+      <ChangeTheme />
+      <Header />
+      <Body />
+      <Footer />
+      {/* <TimerContextProvider>
         <TimersDashboard />
-      </TimerContextProvider>
+      </TimerContextProvider> */}
     </div>
   );
 }
---
+++ day-61-custom-hook/src/components/Body.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import { useFetch } from "../context/UseFetch"
+import { useTheme } from "../context/useTheme"
+
+
+export default function Body() {
+
+    const URL = 'https://dog.ceo/api/breeds/image/random'
+    const { theme, setTheme } = useTheme()
+    const { response, error } = useFetch(URL, {})
+    console.log(response);
+
+    const [styleClass, setStyleClass] = useState()
+
+    useEffect(() => {
+        setStyleClass(`${theme} middle-container`)
+    }, [theme])
+
+    return (
+        <div className={styleClass}>
+            <p>Body</p>
+            {response.message &&
+                <img src={response.message} alt='Dog image' />}
+
+        </div>
+    )
+}
