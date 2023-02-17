2b6144b85ecf31ee85926bf73ed53f43506afe78
Tugsoo19 <tugswork@gmail.com>
2023-02-17 11:12:04 +0800

    day-65

---
+++ day-65-functional-vs-class/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import { ClassCounter } from './components/ClassCounter';
+import { FunctionalCounter } from './components/FunctionalCounter';
+
+import { TimersDashboard } from "./components/TimersDashboard";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Day-65 - class Component - Timer App</h1>
+      {/* <FunctionalCounter />
+      <ClassCounter /> */}
+      <TimersDashboard />
+
+    </div>
+  );
+}
