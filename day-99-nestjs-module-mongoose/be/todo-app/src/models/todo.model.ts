c52f9f1d21220d781cd26234cb46d8b351a8ea32
tugsoo19 <tugswork@gmail.com>
2023-04-18 09:44:15 +0800

    day-99

---
+++ day-99-nestjs-module-mongoose/be/todo-app/src/models/todo.model.ts
@@ -0,0 +1,20 @@
+import * as mongoose from "mongoose"
+
+export const TodoSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    completed: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+})
+
+export interface Todo {
+    id: string;
+    title: string,
+    completed: boolean;
+}
+
