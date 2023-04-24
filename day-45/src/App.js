248e6a2f9501a11ec3aebaa90302fc1b06cf00f0
tugsoo19 <tugswork@gmail.com>
2023-04-24 16:48:40 +0800

    day-45 test hiisen

--- day-45/src/App.js
+++ day-45/src/App.js
@@ -1,27 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import Accordion from './component/Accordion';
-import Index from './component/Index';
+import logo from "./logo.svg";
+import "./App.css";
+import Accordion from "./component/Accordion";
+import Index from "./component/Index";
+import LoginTest from "./component/LoginTest";
 
-import { Routes, Route } from 'react-router-dom'
-import Login from './component/Login';
-import Home from './component/Home';
-import Register from './component/Register';
+import { Routes, Route } from "react-router-dom";
 
+import Home from "./component/Home";
 
-
+import Signup from "./component/SignupTest";
 
 function App() {
   return (
     <div className="App">
-
       <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/' element={<Home />} />
+        <Route path="/login" element={<LoginTest />} />
+        <Route path="/" element={<Home />} />
         {/* <Route path='/' element={<Index />} /> */}
-        <Route path="/accordion" element={<Accordion />} />
-        <Route path="/register" element={<Register />} />
-
+        {/* <Route path="/accordion" element={<Accordion />} /> */}
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   );
--- day-57-context/context-lesson/src/index.js
+++ day-57-context/context-lesson/src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import BooksList from './BooksList';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import BooksList from "./BooksList";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BooksList />
+    <App />
   </React.StrictMode>
 );
 
