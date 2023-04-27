50b3f76408a9f6a0da0de662322ba91e10cee16b
tugsoo19 <tugswork@gmail.com>
2023-04-27 11:58:25 +0800

    sign in and out

--- day-102-oauth-with-nextauth/src/pages/api/auth/[...nextauth].ts
+++ day-102-oauth-with-nextauth/src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,17 @@
 import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github"
+import FaceBookProvider from "next-auth/providers/facebook"
 
 
 
 export const authOptions: NextAuthOptions = {
     providers: [
+
+        FaceBookProvider({
+            clientId: process.env.FACEBOOK_ID,
+            clientSecret: process.env.FACEBOOK_SECRET,
+        }),
 
         GithubProvider({
             clientId: process.env.GITHUB_ID,
